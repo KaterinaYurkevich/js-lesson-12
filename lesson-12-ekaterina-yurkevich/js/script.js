9db5e904f39e2b8832c640003a2cd36e1bdabbc8
KATSIARYNA YURKEVICH <77401014+KaterinaYurkevich@users.noreply.github.com>
2022-06-20 00:20:45 +0300

    Add files via upload

---
+++ lesson-12-ekaterina-yurkevich/js/script.js
@@ -0,0 +1,40 @@
+// 1)
+
+async function loadJson(url) {
+    let response = await fetch(url);
+
+    if (response.status == 200) {
+        return await response.json();
+    } else {
+        throw new Error(response.status);
+    }
+}
+
+loadJson('no-such-user.json')
+    .catch(alert);
+
+
+//2)
+
+async function getData(url) {
+    try {
+        let response = await fetch(url);
+        let data = await response.json();
+        console.log(data);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+getData("https://jsonplaceholder.typicode.com/todos/1");
+
+//2 var
+
+function getDataNew(url) {
+    return new Promise(resolve => resolve())
+        .then(() => fetch(url))
+        .then(response => response.json())
+        .then(data => console.log(data));
+}
+
+getDataNew("https://jsonplaceholder.typicode.com/todos/1");
